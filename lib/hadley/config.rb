8ccf07d34f31700a25d7f25e1e9535882cd82b88
Sean M. Duncan <bitbutcher@gmail.com>
2012-02-13 01:51:57 -0600

    added some documentation in yardoc format

--- lib/hadley/config.rb
+++ lib/hadley/config.rb
@@ -1,28 +1,70 @@
+# This class is a convenience wrapper around an Hash that provides a more expressive api for initial configuration and
+# referencing the configuration information at runtime.  For example:
+#   config.prop 'value' # --> config[:prop] = 'value'
+#   config.prop = 'value' # --> same as above
+#   config.props 'a', 'b', 'c' # --> config[:props] = [ 'a', 'b', 'c' ]
+#   config.props = 'a', 'b', 'c' # same as above
+#   config.callback { |it| puts it } # config[:callback] = { |it| puts it }
+#   config.prop # --> config[:prop]
 class Hadley::Config
 
-  def initialize(config={})
-    @config = config
+  # Initializes this Config with the specified defaults.
+  #
+  # @param [Hash] defaults The default configuration values for this Config instance.
+  def initialize(defaults={})
+    @config = defaults
   end
 
+  # Delegates to {#set}, {#get} or {#proc} depending on the nature of the given name, if a block is given or if the args
+  # array is not empty.
+  # 
+  # @param [String] name The name of the property to be read or written.  If the name ends with '=' it will be stripped
+  #  from the name and the operation will be treated as a write.
+  # @param [*Object] args The optional array of property values to be assigned to the provided property name.  If this
+  #  array is not empty then {#set} will be called.
+  # @param [Proc] &block The optional block to be assigned to the provided property name. If the operation has a block 
+  #  given then {#proc} will be called.
+  #
+  # @return [Object,nil] The value ultimately written to or read from the given property name.
   def method_missing(name, *args, &block)
     if block_given?
       proc(name, &block)
     elsif name =~ /(.+)=$/
-      set($1, *args, &block)
+      set($1, *args)
+    elsif not args.empty?
+      set(name, *args)
     else
-      get(name, &block)
+      get(name)
     end
   end
 
+  # Stores the given block under the provided property name.
+  #
+  # @param [String] name The name of the property to be written.
+  # @param [Proc] &block The block to be assigned to the provided property name.
+  #
+  # @return [Proc] The block written to the provided name.
   def proc(name, &block)
     @config[name.to_sym] = block
   end
 
+  # Stores the value or values indicated by the args array under the provided property name.
+  #
+  # @param [String] name The name of the property to be written.
+  # @param [*Object] args The value or values to be assigned to the provided property name. If a single value is found
+  #  a scalar will be written otherwise an array will be written.
+  #
+  # @return [Object,nil] The value written to the provided name.
   def set(name, *args)
     @config[name.to_sym] = args.size == 1 ? args.first : args
   end
 
-  def get(name, &block)
+  # Retrieves the value stored under the provided name.
+  #
+  # @param [String] name The name of the property to be read.
+  #
+  # @return [Object,nil] The value stored under the provided name or nil if no such value exists.
+  def get(name)
     @config[name.to_sym]
   end
 
--- lib/hadley/token_store.rb
+++ lib/hadley/token_store.rb
@@ -1,13 +1,23 @@
+# This class handles the storage, retrieval and removal of OAuth 2 bearer tokens sent from the AFID authorization 
+# server. The TokenStore delegates most of the work to the delegate store, which must support the api set forth by
+# ActiveSupport::Cache::Store.
 class Hadley::TokenStore
 
+  # This method initializes the TokenStore with the delegate store.
+  #
+  # @param [ActiveSupport::Cache::Store] store The TokenStore instance will delegate the heavy lifting to the provided 
+  #  store.
   def initialize(store)
     @store = store
   end
 
-  def key_for(token)
-    "afid-access-token:#{token}"
-  end
-
+  # This method retrieves the AFID identity information associated with the provided token.  If no such identity is 
+  # found the result will be nil.
+  #
+  # @param [String] token The unique token provisioned by the AFID resource server.
+  #
+  # @return [Hash, nil] A Hash representation of the identity associated with the provided token or nil if no such identity
+  #  exists.
   def get(token)
     access = @store.read(key_for(token))
     if access
@@ -16,12 +26,37 @@ class Hadley::TokenStore
     access
   end
 
+  # This method stores the provided AFID identity information under the given AFID token for the duration of time
+  # specified by the expires_in argument.
+  # 
+  # @param [String] token The unique token provisioned by the AFID resource server.
+  # @param [Integer] expires_in The duration of time (in seconds) that the provided AFID identity information should be 
+  #  stored.
+  # @param [Hash] data The identity information to be assiciated with the given AFID token.
+  #
+  # @return [Boolean, nil] True if and only if the identity information was stored successfully.
   def put(token, expires_in, data={})
     @store.write(key_for(token), data, expires_in: expires_in)
   end
 
+  # This method removes the AFID identity information associated with the provided token.
+  #
+  # @param [String] token The token provisioned by the AFID resource server.
+  #
+  # @return [Boolean, nil] True if an only if the identity information was removed successfully.
   def delete(token)
     @store.delete(key_for(token))
+  end
+
+  protected
+
+  # This method derives the appropriate datastore key from the given AFID token.
+  #
+  # @param [String] token The unique token provisioned by the AFID resource server.
+  #
+  # @return [Symbol] The appropriate datastore key for the given AFID token.
+  def key_for(token)
+    "afid-access-token:#{token}".to_sym
   end
 
 end
